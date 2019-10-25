8d924a407c2a9c67695e50a227497fa63b15ea74
woohyeonjo <hyeon930@gmail.com>
2019-10-25 10:06:27 +0900

    cicd test

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>CICD 오늘로써 마무리 합니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
+    <h1>CI/CD 테스트 중입니다.</h1>
   </div>
 </template>
