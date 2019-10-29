ecf52e30b22073f328aea5eab332d4a5d8e92ea0
sssssseungmi <tmdal9516@naver.com>
2019-10-29 10:14:02 +0900

    add async await'

--- client/src/utils/firebaseUtils.js
+++ client/src/utils/firebaseUtils.js
@@ -12,65 +12,64 @@ const firebaseConfig = {
 }
 firebase.initializeApp(firebaseConfig)
 
-const user = {email: "tmdal9516@naver.com", displayName: "최승미"};
+const user = { email: "tmdal9516@naver.com", displayName: "최승미" };
 const db = firebase.firestore();
 
 
 // firebase.auth().currentUser를 userStore에 저장된 user정보로 바꿔야함
 const dbCRUD = {
-    makeChatRoom(roomName, gpsX, gpsY){
+    makeChatRoom(roomName, gpsX, gpsY) {
         // let user = firebase.auth().currentUser;
-		db.collection('chatRoomList').add({
-			roomName: roomName,
-			roomGpsX: gpsX,
-			roomGpsY: gpsY,
-			roomHost: user.email,
-        }).then((docRef)=>{
+        db.collection('chatRoomList').add({
+            roomName: roomName,
+            roomGpsX: gpsX,
+            roomGpsY: gpsY,
+            roomHost: user.email,
+        }).then((docRef) => {
             console.log(docRef);
             db.collection('user').doc(user.email).update({
-                userHostRoom : docRef.id,
-                userGuestRoom : docRef.id
+                userHostRoom: docRef.id,
+                userGuestRoom: docRef.id
             });
         });
     },
-    // 일단 userGuestRoom을 roomId(String)로
-	getUserGuestRoom(){
-		// let user = firebase.auth().currentUser;
-		this.db.collection('user').doc(user.email).get().then(documentSnapshot => {
-			let data = documentSnapshot.data();
-			return data.userGuestRoom;
-		});
-    },
-    joinRoom(roomId){
+    // 일단                                                                                                                           userGuestRoom을 roomId(String)로
+    getUserGuestRoom() {
         // let user = firebase.auth().currentUser;
-        this.db.collection('user').doc(user.email).update({userGuestRoom : roomId});
-        this.db.collection('chatRoomList').doc(roomId).collection('roomJoinUser').doc(user.email).set({
+        db.collection('user').doc(user.email).get().then(documentSnapshot => {
+            let data = documentSnapshot.data();
+            return data.userGuestRoom;
+        });
+    },
+    async joinRoom(roomId) {
+        // let user = firebase.auth().currentUser;
+        await db.collection('user').doc(user.email).update({ userGuestRoom: roomId });
+        await db.collection('chatRoomList').doc(roomId).collection('roomJoinUser').doc(user.email).set({
             userName: user.displayName
         });
     },
-    sendMessage(message){
+    sendMessage(message) {
         // let sender = firebase.auth().currentUser;
         let roomId = getUserGuestRoom();
-        this.db.collection('chatRoomList').doc(roomId).collection('messages').add({
+        db.collection('chatRoomList').doc(roomId).collection('messages').add({
             msg: message,
             sender: sender.displayName,
             createdAt: new Date()
         });
     },
-    fetchMessage(){
+    fetchMessage() {
         return new Promise((resolve, reject) => {
-            this.db.collection('chat').orderBy('createdAt').onSnapshot((querySnapshot)=>{
-              let allMessages=[];
-              querySnapshot.forEach(doc=>{
-                let docu = doc.data();
-                docu.id = doc.id;
-                allMessages.push(docu);
-              })
-              store.commit('setMessages',allMessages);
+            db.collection('chat').orderBy('createdAt').onSnapshot((querySnapshot) => {
+                let allMessages = [];
+                querySnapshot.forEach(doc => {
+                    let docu = doc.data();
+                    docu.id = doc.id;
+                    allMessages.push(docu);
+                })
+                store.commit('setMessages', allMessages);
             })
         });
     }
 }
 
 export default dbCRUD
-
