205fe6c5aa9ddcb9ff97f5588c5ded601bb647bf
yun <ybchae0801@naver.com>
2019-10-30 14:46:22 +0900

    채팅방 나가기 확인 알림

--- client/src/components/BackButton.vue
+++ client/src/components/BackButton.vue
@@ -1,36 +1,69 @@
 <template>
 	<div class="buttonWrapper">
 		<v-btn 
-			id="button" icon large
-			@click="exit"
+			id="button" icon large color="white"
+			@click="dialog=true"
 			@mouseover="mouseover"
             @mouseout="mouseout"
 		>
 			<v-icon>mdi-chevron-double-left</v-icon>
 		</v-btn>
-		<span v-if="show">채팅방 나가기</span>
+		<span v-if="show" style="color:white">나가기</span>
+		<v-dialog
+			v-model="dialog"
+			max-width="390"
+		>
+			<v-card>
+			<v-card-title class="mb-5">채팅방을 나가시겠습니까?</v-card-title>
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn
+					color="primary darken-1"
+					text
+					@click="exit"
+				>
+					Yes
+				</v-btn>
+				<v-btn
+					color="red darken-1"
+					text
+					@click="dialog = false"
+				>
+					No
+				</v-btn>
+			</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</div>
 </template>
 <script>
 export default {
 	data() {
 		return {
-			show: false
+			show: false,
+			dialog: false
 		}
 	},
 	methods: {
+		setButtonOpacity(val) {
+			document.getElementById('button').style.opacity=val;
+		},
 		mouseover(){
 			this.show = true;
-			document.getElementById('button').style.color="black";
+			this.setButtonOpacity(1.0);
 		},
 		mouseout(){
 			this.show = false;
-			document.getElementById('button').style.color='grey';
+			this.setButtonOpacity(0.4);
 		},
 		exit(){
 			// 나가기전에 뭔가 해야 될 듯...
+			this.dialog = true;
 			this.$router.go(-1);
 		}
+	},
+	mounted() {
+		this.setButtonOpacity(0.4);
 	}
 }
 </script>
